a0aba2a4d3b559402c6a6d783f55b1677dd3d0ae
brittleru <diemetal851@gmail.com>
2025-06-20 08:03:23 +0300

    Updated logger for offline student.

--- auto_follow/visualization/load_data.py
+++ auto_follow/visualization/load_data.py
@@ -7,6 +7,30 @@ import pandas as pd
 from auto_follow.visualization.metadata_trajectory_analysis import calculate_drone_distance
 
 
+class ConfigsDirName:
+    SIM = [
+        "bunker-online-4k-config-test-down-left",
+        "bunker-online-4k-config-test-down-right",
+        "bunker-online-4k-config-test-left",
+        "bunker-online-4k-config-test-right",
+        "bunker-online-4k-config-test-up-left",
+        "bunker-online-4k-config-test-up-right",
+        "bunker-online-4k-config-test-front-small-offset-right",
+        "bunker-online-4k-config-test-front-small-offset-left"
+    ]
+
+    REAL = [
+        "real-ibvs-down-left",
+        "real-ibvs-front-small-offset-left",
+        "real-ibvs-left",
+        "real-ibvs-up-left",
+        "real-ibvs-down-right",
+        "real-ibvs-front-small-offset-right",
+        "real-ibvs-right",
+        "real-ibvs-up-right",
+    ]
+
+
 def extract_direction(config_name: str) -> str:
     direction_keywords = [
         "front-small-offset-right",
@@ -83,14 +107,11 @@ def load_json_metadata(results_dir: str | Path, scene_dir_names: list[str]) -> p
     return pd.DataFrame(all_metadata)
 
 
-def load_parquet_data(results_dir: str | Path, scene_dir_names: list[str], is_student=False):
+def load_parquet_data(results_dir: str | Path, scene_dir_names: list[str], is_student: bool = False):
     all_logs = []
     results_dir = Path(results_dir)
 
     parquet_log_name = "logs.parquet"
-    if is_student:
-        parquet_log_name = "logs-teacher-output.parquet"
-
     for scene_name in scene_dir_names:
         parquet_logs_path = results_dir / scene_name / "parquet-logs"
         if not parquet_logs_path.exists():
@@ -100,15 +121,34 @@ def load_parquet_data(results_dir: str | Path, scene_dir_names: list[str], is_st
         scene_direction = extract_direction(scene_name)
         for experiment_run in os.listdir(parquet_logs_path):
             parquet_file = parquet_logs_path / experiment_run / parquet_log_name
-            if parquet_file.exists():
-                df = pd.read_parquet(parquet_file)
-                df["run"] = experiment_run
-                df["scene"] = scene_name
-                df["direction"] = scene_direction
-                all_logs.append(df)
+            if not parquet_file.exists():
+                print(f"No parquet file for {parquet_file} in {experiment_run}")
+                continue
+
+            df = pd.read_parquet(parquet_file)
+
+            if is_student:
+                parquet_teacher_output = parquet_logs_path / experiment_run / "logs-teacher-output.parquet"
+                if not parquet_teacher_output.exists():
+                    print(f"Non existent parquet file: {parquet_teacher_output}")
+                    continue
+                try:
+                    df_teacher = pd.read_parquet(parquet_teacher_output)
+                    df_teacher = df_teacher[[
+                        'timestamp', 'frame_idx', 'jacobian_matrix', 'jcond',
+                        'current_points_flatten', 'goal_points_flatten', 'err_uv', 'velocity'
+                    ]]
+                except Exception as e:
+                    print(f"Error reading parquet file:\n{e}\n{parquet_teacher_output}")
+                    continue
+                df = df.merge(df_teacher, on=['timestamp', 'frame_idx'], how='inner')
+
+            df["run"] = experiment_run
+            df["scene"] = scene_name
+            df["direction"] = scene_direction
+            all_logs.append(df)
 
     return pd.concat(all_logs, ignore_index=True)
-
 
 
 if __name__ == '__main__':
